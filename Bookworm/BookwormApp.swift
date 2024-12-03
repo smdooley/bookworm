6a7317b19b206a40811f38b6717d54cf64d09b54
Sean Dooley <s.dooley@ctidigital.com>
2024-12-03 10:56:31 +0000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sean Dooley on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
